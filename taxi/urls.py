e45348a68fdeb2ddf5e94f8b237500bc8d8b2031
Vitalii Prushak <48731758+Gheifr@users.noreply.github.com>
2025-12-02 14:37:36 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    index,
+    CarsListView,
+    CarDetailView,
+    DriversListView,
+    ManufacturersListView,
+    DriverDetailView,
+)
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", index,
+         name="index"),
+    path("cars/", CarsListView.as_view(),
+         name="car-list"),
+    path("drivers/", DriversListView.as_view(),
+         name="driver-list"),
+    path("manufacturers/", ManufacturersListView.as_view(),
+         name="manufacturer-list"),
+    path("cars/<int:pk>", CarDetailView.as_view(),
+         name="car-detail"),
+    path("drivers/<int:pk>", DriverDetailView.as_view(),
+         name="driver-detail"),
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpRequest, Http404
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +15,53 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class CarsListView(generic.ListView):
+    model = Car
+    context_object_name = "car_list"
+    paginate_by = 5
+    queryset = Car.objects.select_related("manufacturer")
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class DriversListView(generic.ListView):
+    model = Driver
+    context_object_name = "driver_list"
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
+
+
+class ManufacturersListView(generic.ListView):
+    model = Manufacturer
+    context_object_name = "manufacturer_list"
+    queryset = Manufacturer.objects.all()
+    paginate_by = 5
+
+
+# Function based view:
+# def car_list_view(request: HttpRequest):
+#     context = {
+#         "car_list": Car.objects.all(),
+#     }
+#
+#     return render(request, "taxi_service/car_list.html", context=context)
+
+
+# def car_detail_view(request, pk: int):
+#     try:
+#         car = Car.objects.get(id=pk)
+#     except Car.DoesNotExist:
+#         raise Http404("Car does not exist")
+#     context = {
+#         "car": car,
+#     }
+#
+#     return render(request, "taxi_service/car_detail.html", context=context)
