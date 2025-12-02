39684f168d39487ad4c14442ac7e7dbb8a9bdc5c
Vitalii Prushak <48731758+Gheifr@users.noreply.github.com>
2025-12-02 15:02:44 +0200

    Indend update

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -20,9 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = (
-        [
-            path("admin/", admin.site.urls),
-            path("", include("taxi.urls", namespace="taxi")),
-        ]
-        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    [
+        path("admin/", admin.site.urls),
+        path("", include("taxi.urls", namespace="taxi")),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 )
